e7dc0136178b491cb62b126bebfe7602efd609c3
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-29 15:22:46 -0300

    emailverificationsend

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
 import { CreateUserStreamer, CreateUserViewer } from "#/@types/user"
 import userStreamer from "#/models/userStreamer"
-
+import nodemailer from 'nodemailer'
 import userViewer from "#/models/userViewer"
 import { CreateUserStreamerSchema, CreateUserViewerSchema } from "#/utils/validationSchema"
 import { RequestHandler } from "express"
+import { GOOGLE_USER, GOOGLE_PASS } from "#/utils/variables"
 
 export const createViewerUser: RequestHandler = async (req: CreateUserViewer, res) => {
     const { email, password, name } = req.body
@@ -12,7 +13,24 @@ export const createViewerUser: RequestHandler = async (req: CreateUserViewer, re
     })
 
     const userviwer = await userViewer.create({ name, email, password })
-    res.json({ userviwer })
+
+    //send verification email
+
+    const transport = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+          user: GOOGLE_USER,
+          pass: GOOGLE_PASS
+        }
+      });
+
+      transport.sendMail({
+        to: userviwer.email,
+        from: "auth@yapp.com",
+        html: "<h1>123445</h1>"
+      })
+    res.status(201).json({ userviwer })
+
 
 }
 
@@ -23,7 +41,22 @@ export const createStreamerUser: RequestHandler = async (req: CreateUserStreamer
     })
 
     const userstreamer = await userStreamer.create({ name, email, password, cpf, phoneNumber, address })
-    res.json({ userstreamer })
+
+    //send verification email
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: GOOGLE_USER,
+          pass: GOOGLE_PASS
+        }
+      });
+
+      transport.sendMail({
+        to: userstreamer.email,
+        from: "auth@yapp.com",
+        html: "<h1>123445</h1>"
+      })
+    res.status(201).json({ userstreamer })
 
 }
 
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,4 +1,3 @@
 const {env} = process as { env: {[key: string]: string}}
 
-export const MONGO_URI = env.MONGO_URI
- 
+export const {MONGO_URI, GOOGLE_USER, GOOGLE_PASS} = env 
