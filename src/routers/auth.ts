e43d958b7766c250c658c4dd3b9c551dd34c15f0
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-12-12 16:58:53 -0300

    signAuthorization-headers-token

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -5,6 +5,8 @@ import { validate } from '#/middleware/validator'
 import { CreateUserStreamerSchema, CreateUserViewerSchema, SignInValidationSchema, EmailVerificationBody as TokenAndIDValidation, updatePasswordSchema } from '#/utils/validationSchema'
 import { createStreamerUser, createViewerUser, generateForgetPasswordLink, grantValid, sendReVerificationTokenStreamer, sendReVerificationTokenViewer, signInStreamer, signInViewer, updateStreamerPassword, updateViewerPassword, verifyEmail } from '#/controllers/users'
 import { isValidPassResetToken } from '#/middleware/auth'
+import { JwtPayload, verify } from 'jsonwebtoken'
+import { JWT_SECRET } from '#/utils/variables'
 
 const router = Router()
 router.post(
@@ -27,6 +29,62 @@ router.post('/update-password', validate(updatePasswordSchema), isValidPassReset
 router.post('/sign-in',
  validate(SignInValidationSchema),
  signInViewer, signInStreamer)
+
+
+ router.get('/is-auth-UserViewer', async (req, res) => {
+    const {authorization} = (req.headers)
+    const token = authorization?.split("Bearer ")[1]
+    if(!token) res.status(403).json({error: "Unauthorized request!" })
+
+    const payload = verify(token, JWT_SECRET) as JwtPayload
+    const id = payload.userId
+
+    const userviewer = await userViewer.findById(id)
+    const userstreamer = await userViewer.findById(id)
+    if(!userviewer) return res.status(403).json({error: "Unauthorized request!"})
+
+    res.json({
+        profile: { 
+            id: userviewer._id, 
+            name: userviewer.name, 
+            email: userviewer.email, 
+            verified: userviewer.verified, 
+            avatar: userviewer.profilePicture?.url, 
+            followers: userviewer.followers.length, 
+            followings: userviewer.followings.length 
+        },
+        token
+    })
+})
+
+router.get('/is-auth-UserStreamer', async (req, res) => {
+    const {authorization} = (req.headers)
+    const token = authorization?.split("Bearer ")[1]
+    if(!token) res.status(403).json({error: "Unauthorized request!" })
+
+    const payload = verify(token, JWT_SECRET) as JwtPayload
+    const id = payload.userId
+
+
+    const userstreamer = await userStreamer.findById(id)
+    if(!userstreamer) return res.status(403).json({error: "Unauthorized request!"})
+
+    res.json({
+        profile: { 
+            id: userstreamer._id, 
+            name: userstreamer.name, 
+            email: userstreamer.email, 
+            verified: userstreamer.verified, 
+            avatar: userstreamer.profilePicture?.url, 
+            followers: userstreamer.followers.length, 
+            followings: userstreamer.followings.length 
+        },
+    })
+})
+
+
+
+
 
 
 export default router
